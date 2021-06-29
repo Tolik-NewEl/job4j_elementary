f2b8f4023cefe6b3ae85ea3173b26a74913634cf
Tolik-NewEl <tsepushtanov@outlook.com>
2021-06-29 15:12:10 +0500

    9.2. Checkstyle [#4123]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Anatoliy Tsepushtanov");
+        System.out.println("29.05.1984");
+    }
+}
