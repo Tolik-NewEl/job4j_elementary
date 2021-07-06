84ee0a54d831a8a4ddc75608633a75c478a347ca
Tolik-NewEl <tsepushtanov@outlook.com>
2021-07-06 18:21:37 +0500

    5.5. Простое число [#171694 #174739]

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -4,13 +4,10 @@ public class CheckPrimeNumber {
     public static boolean check(int number) {
         boolean prime = number > 1;
         for (int index = 2; index < number; index++) {
-            if (prime = false) {
-                break;
+            if (number % index == 0) {
+            prime = false;
+            break;
             }
-            else if (number % index == 0) {
-                break;
-            }
-            prime = true;
         }
         return prime;
     }
