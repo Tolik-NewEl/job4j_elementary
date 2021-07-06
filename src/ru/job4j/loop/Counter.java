4086abeb4a4dd5c914bc3d0c077f336c47a62320
Tolik-NewEl <tsepushtanov@outlook.com>
2021-07-06 17:44:07 +0500

    5.8. Именование классов и переменных [#4087]

--- src/ru/job4j/loop/Counter.java
+++ src/ru/job4j/loop/Counter.java
@@ -13,11 +13,12 @@ public class Counter {
         int sum = 0;
         for (int index = start; index <= finish; index++) {
             if (index % 2 == 0) {
-                sum = sum + index ;
+                sum = sum + index;
             }
         }
         return sum;
     }
+
     public static void main(String[] args) {
         System.out.println(sum(0, 10));
         System.out.println(sum(3, 8));
